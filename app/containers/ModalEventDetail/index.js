219b557f2235d6f323bb8be890e66f2bee48333e
Roman Bednyakov <rbednyakov@mentalstack.com>
2018-10-03 14:54:47 +0300

    css

--- app/containers/ModalEventDetail/index.js
+++ app/containers/ModalEventDetail/index.js
@@ -9,12 +9,12 @@ const ModalEventDetail = (props) => {
     <div className="modalEventDetail">
       <span className="modalEventDetail__title">{eventName}</span>
       <span className="modalEventDetail__details"><span className="modalEventDetail__modify">Coordinator:</span> {coordinator}</span>
-      <span className="modalEventDetail__details"><span className="modalEventDetail__modify"> Email:</span>{email}</span>
-      <span className="modalEventDetail__details"><span className="modalEventDetail__modify"> Phone:</span> {phone}</span>
-      <span className="modalEventDetail__details"><span className="modalEventDetail__modify"> Venue:</span> {venue}</span>
-      <span className="modalEventDetail__details"><span className="modalEventDetail__modify"> Venue City:</span> {venueCity}</span>
-      <span className="modalEventDetail__details"><span className="modalEventDetail__modify"> Venue State:</span> {venueState}</span>
-      <span className="modalEventDetail__details"><span className="modalEventDetail__modify"> Venue Street:</span> {venueStreet1}</span>
+      <span className="modalEventDetail__details"><span className="modalEventDetail__modify">Email:</span> {email}</span>
+      <span className="modalEventDetail__details"><span className="modalEventDetail__modify">Phone:</span> {phone}</span>
+      <span className="modalEventDetail__details"><span className="modalEventDetail__modify">Venue:</span> {venue}</span>
+      <span className="modalEventDetail__details"><span className="modalEventDetail__modify">Venue City:</span> {venueCity}</span>
+      <span className="modalEventDetail__details"><span className="modalEventDetail__modify">Venue State:</span> {venueState}</span>
+      <span className="modalEventDetail__details"><span className="modalEventDetail__modify">Venue Street:</span> {venueStreet1}</span>
     </div>
   );
 };
